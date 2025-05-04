db3ec882137d669d8333ba31464fc2ca54f9cdfd
RafaelHFSousa <rafaelholandafs@edu.unifor.br>
2025-05-04 11:25:47 -0300

    aula parte 2

--- src/routes/alunos.ts
+++ src/routes/alunos.ts
@@ -5,6 +5,7 @@ const prisma = new PrismaClient();
 
 export async function alunoRoutes(app: FastifyInstance) {
   app.post("/alunos", async (request: FastifyRequest, reply: FastifyReply) => {
+    console.log("--");
     const { matricula, nome, idade, curso } = request.body as {
       matricula: string;
       nome: string;
@@ -13,12 +14,7 @@ export async function alunoRoutes(app: FastifyInstance) {
     };
 
     const aluno = await prisma.aluno.create({
-      data: {
-        matricula,
-        nome,
-        idade,
-        curso,
-      },
+      data: { matricula, nome, idade, curso },
     });
 
     return reply.status(200).send(aluno);
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import Fastify, { FastifyReply, FastifyRequest } from "fastify";
-
+import Fastify, { FastifyReply, FastifyRequest } from "fastify"
+import { alunoRoutes } from "./routes/alunos"
 
 const app = Fastify()
 
+app.register(alunoRoutes)
 
-app.get("/alunos", (request: FastifyRequest, reply: FastifyReply) => {
-  return {"message": "ola mundoo"}
-})
-
+// app.get("/alunos", (request: FastifyRequest, reply: FastifyReply) => {
+//   return {"message": "Ok"}
+// })
 
 app.listen({port: 3333}, () => {
-  console.log("Server rodando na porta 3333!")
+  console.log('Servidor rodando na porta 3333!')
 })
